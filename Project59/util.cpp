7673a4615088001414514fcbf5a43baa58498589
Dmitry <dmitrysamisenko@gmail.com>
2026-05-31 17:58:25 +0300

    Add project files.

---
+++ Project59/util.cpp
@@ -0,0 +1,26 @@
+#include "util.h"
+
+void print(string text) {
+	cout << text;
+}
+void init(int matrix[N_SIZE][M_SIZE], int n, int m, int a, int b) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) {
+			matrix[i][j] = rand() % (b - a + 1) + a;
+		}
+	}
+}
+string convert(int matrix[N_SIZE][M_SIZE], int n, int m) {
+	string s = "";
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++) {
+			s += " " + to_string(matrix[i][j]);
+		}
+		s += "\n";
+	}
+
+	return s;
+}
---
+++ Project59/util.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+#define N_SIZE 10
+#define M_SIZE 10
+
+using namespace std;
+
+void print(string text);
+void init(int matrix[N_SIZE][M_SIZE], int n, int m, int a, int b);
+string convert(int matrix[N_SIZE][M_SIZE], int n, int m);
